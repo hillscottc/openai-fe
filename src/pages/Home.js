305113b90d82ea62a2cc20036c46672028952240
Scott Hill <scott289@gmail.com>
2023-07-26 14:40:36 -0700

    welcome

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import Typography from "@mui/material/Typography";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <main>
       <Typography variant="h1" gutterBottom>
         <DashboardIcon fontSize={"inherit"} /> Welcome
       </Typography>
+        Check out the <Link to={"/rap"}>Rap Battle Bot</Link> .
     </main>
   );
 }
